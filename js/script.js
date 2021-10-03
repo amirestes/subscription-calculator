f9d10f0752b73b0dd5ec9ee81f5f7ec18a3d740b
amirestes <amirestes@gmail.com>
2021-10-03 09:16:30 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// Your JS code here
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = "1";
+
+subTypeElement.addEventListener("change", function (e) {
+  subType = e.target.value;
+  if (subType === "basic") {
+    //console.log(subType);
+  } else if (subType === "standard") {
+    //console.log(subType);
+  } else {
+    //console.log(subType);
+  }
+  updateSubscriptionDiv();
+});
+
+subDurationElement.addEventListener("change", function (e) {
+  subDuration = Number(e.target.value);
+  if (subDuration === "1") {
+    //console.log(subDuration);
+  } else if (subDuration === "6") {
+    //console.log(subDuration);
+  } else {
+    //console.log(subDuration);
+  }
+  updateSubscriptionDiv();
+});
+
+var updateSubscriptionDiv = function () {
+  var monthlyCost = "5";
+  if (subType === "standard") {
+    monthlyCost = "7";
+  } else if (subType === "premium") {
+    monthlyCost = "10";
+  }
+  var total = subDuration * monthlyCost;
+  result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
